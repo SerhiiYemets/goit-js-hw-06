8ca65823ef36fe00ecdb70626b72b1821138e115
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-09-08 09:40:02 +0200

    add js file

--- js/task-2.js
+++ js/task-2.js
@@ -1,36 +1,36 @@
-class Storage {
-    #items;
-    constructor(arg) {
-        this.#items = arg;
-    }
+// class Storage {
+//     #items;
+//     constructor(arg) {
+//         this.#items = arg;
+//     }
     
-    getItems() {
-        return this.#items;
-    }
+//     getItems() {
+//         return this.#items;
+//     }
     
-    addItem(newItem) {
-        this.#items.push(newItem);
-    }
+//     addItem(newItem) {
+//         this.#items.push(newItem);
+//     }
     
-    removeItem(itemToRemove) {
-        const polo = this.#items.indexOf(itemToRemove);
-        if (polo !== -1) {
-            this.#items.splice(polo, 1);
-        }
-    }
-}
+//     removeItem(itemToRemove) {
+//         const polo = this.#items.indexOf(itemToRemove);
+//         if (polo !== -1) {
+//             this.#items.splice(polo, 1);
+//         }
+//     }
+// }
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
-storage.removeItem("Scaner");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+// storage.removeItem("Scaner");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 
 
